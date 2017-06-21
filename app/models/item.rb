cf5448b5fe63df7b305c071fd26ebf8af1521fd2
Shohey Tanaka <shohei-tanaka@dac.co.jp>
2017-06-21 05:32:28 +0000

    modified

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,5 +12,5 @@ class Item < ApplicationRecord
   has_many :want_users, through: :wants, class_name: 'User', source: :user
   
   has_many :haves
-  has_many :have_users, through: :haves, class_name: 'User', source: :user
+  has_many :have_users, class_name: 'Have', through: :haves, class_name: 'User', source: :user
 end
